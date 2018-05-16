f8c1f7f4e30c85e5c87545ce7e74e078c36cd8b2
鵜巷 <u@chimata.org>
2018-05-17 07:33:34 +0900

    How to install
    - README.md
        - インストールの手順書いた
        - 設定に追加したもの書いた
        - 手順に従うとAnsibleのドキュメントみたいなやつが自動ビルド環境で書けるようになる

---
+++ README.md
@@ -0,0 +1,66 @@
+# Hello Sphinx
+
+## Install
+
+```sh
+$ pip sphinx recommonmark sphinx_rtd_theme sphinx-autobuild nbsphinx sphinx_fontawesome
+```
+
+## Setting
+```sh
+$ sphinx-quickstart
+```
+
+and add following text to `conf.py`
+
+```py
+import sphinx_fontawesome
+extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_fontawesome',
+]
+
+source_suffix = ['.rst', '.md']
+
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+from recommonmark.transform import AutoStructify
+
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
+```
+
+### Build
+```sh
+$ make html
+```
+
+enjoy!
+
+## Auto build
+To build html automatically, add following text to `Makefile`
+
+```makefile
+livehtml:
+	sphinx-autobuild -b html $(SOURCEDIR) $(BUILDDIR)/html
+```
+
+and command `make livehtml`
--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,11 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import sphinx_fontawesome
 extensions = [
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+    'sphinx_fontawesome',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,8 +51,8 @@ templates_path = ['.templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -63,7 +67,7 @@ language = 'ja'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
-exclude_patterns = []
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -74,7 +78,12 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -174,3 +183,19 @@ epub_copyright = copyright
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+from recommonmark.transform import AutoStructify
+
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
